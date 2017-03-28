9c10d2bb465f258a5a7621dfa95077f72f225492
Trevor Riddle <Trevor@Starts-MacBook-Pro.local>
2017-03-28 18:21:38 -0400

    everything

---
+++ WTWApp2/MenuTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WTWApp2
+//
+//  Created by Trevor Riddle on 3/20/17.
+//  Copyright © 2017 Trevor. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewController: UITableViewController {
+    
+    /*Outlets*/
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    /*Actions*/
+    
+    
+}
